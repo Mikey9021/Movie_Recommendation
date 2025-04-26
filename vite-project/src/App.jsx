730883cae9642900e4c82d362a127701e2a7ee31
mikey9021 <mokonadu9021@gmail.com>
2025-04-26 15:25:43 +0000

    Initial project setup with Vite and files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    return (
+        <main>
+            <div className="pattern"/>
+
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="hero image"/>
+                    <h1>Find <span className="text-gradient">Movies</span>  You'll Love Without the Hassle</h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+
+        </main>
+    )
+}
+export default App
